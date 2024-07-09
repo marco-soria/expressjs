36d45da346db44592b1fab7c8a7f1e10266bf105
marco-soria <marco.soriaa7@gmail.com>
2024-07-09 18:00:57 -0500

    get again

--- 01_express/app.js
+++ 01_express/app.js
@@ -2,13 +2,31 @@ import express from 'express';
 
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
+
+// app.get('/api/courses/:id', (req, res) => {
+//     courses.find(course => {
+//         if (course.id === parseInt(req.params.id)) {
+//             res.send(course);
+//         } else {
+//             res.status(404).send('The course with the given ID was not found');
+//         }
+//     });
+// });
+
+
 
 const PORT = process.env.PORT || 3000;
 
